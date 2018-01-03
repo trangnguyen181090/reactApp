fc3c725bb6b9b2fd5e989f17d8cd76bdfacf6609
VDC\tnguyen450 <tnguyen450@csc.com>
2018-01-03 16:27:01 +0700

    edit webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var DIST_PATH = path.resolve(__dirname, 'dist');
+var SOURCE_PATH = path.resolve(__dirname, 'src');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: DIST_PATH,
+        filename: 'app.dist.js',
+        publicPath: '/src/'
+    },
+    devServer: {
+        inline: true,
+        port: 9090
+    },
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: [
+                    'es2015',
+                    'react'
+                ]
+            }
+        }, {
+            test: /\.css$/,
+            loader: 'css-loader',
+            exclude: /node_modules/
+        }]
+    }
+};
